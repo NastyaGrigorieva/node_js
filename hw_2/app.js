6268eb05328d92226818b8d5e90b17e39639a778
NastyaGrigorieva <nastya.grigoreva1502@gmail.com>
2022-02-17 18:11:41 +0200

    committ

--- hw_2/app.js
+++ hw_2/app.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const { engine } = require('express-handlebars');
+const {engine} = require('express-handlebars');
 const path = require('path');
 const hbs = require('express-handlebars');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'static')));
 
 app.set('view engine', '.hbs');
-app.engine('.hbs', engine({ defaultLayout: false }));
+app.engine('.hbs', engine({defaultLayout: false}));
 app.set('views', path.join(__dirname, 'static'));
 
 const users = [];
@@ -19,7 +19,7 @@ app.get('/login', (request, response) => {
     response.render('login');
 })
 
-app.post('/login', ({ body }, response) => {
+app.post('/login', ({body}, response) => {
     const userExist = users.some(user => user.email === body.email);
     if (userExist) {
         error = '!!!!!';
@@ -27,11 +27,11 @@ app.post('/login', ({ body }, response) => {
         return;
     }
 
-    users.push({ ...body, id: users.length});
+    users.push({...body, id: users.length});
     response.redirect('/users');
 });
 
-app.get('/users', ({ query }, response) => {
+app.get('/users', ({query}, response) => {
     if (Object.keys(query).length) {
         let usersArr = [...users];
         if (query.city) {
@@ -41,14 +41,14 @@ app.get('/users', ({ query }, response) => {
             usersArr = usersArr.filter(user => user.age === query.age);
         }
 
-        response.render('users', { users: usersArr });
+        response.render('users', {users: usersArr});
         return;
     }
 
-    response.render('users', { users });
+    response.render('users', {users});
 });
 
-app.get('/users/:id', ({ params }, response) => {
+app.get('/users/:id', ({params}, response) => {
     const user = users.find(user => user.id === +params.id);
     if (!user) {
         error = `!!!!!!`;
@@ -56,11 +56,25 @@ app.get('/users/:id', ({ params }, response) => {
         return;
     }
 
-    response.render('userDetails', { user });
+    response.render('userDetails', {user});
 });
 
 app.get('/error', (request, response) => {
-    response.render('error', { error });
+    response.render('error', {error});
+});
+
+app.get('/signIn', (request, response) => {
+    response.render('signIn')
+});
+app.post('/signIn', ({body}, response) => {
+    const user = users.find(user => user.email === body.email && user.password === body.password);
+    if (!user) {
+        error = `email або password is Ашибка`;
+        response.redirect('/error');
+        return;
+    }
+
+    response.redirect(`/users/${user.id}`)
 });
 
 app.use((request, response) => {
